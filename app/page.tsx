565813dae69585a85a7618af06747d4ff302c1b8
Albert Onyishi <albertonyishi1@gmail.com>
2024-12-17 20:05:13 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+"use client";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import { Provider } from "react-redux";
-import  { store} from "./Store/store"
+import { store } from "./Store/store";
 
 export default function Page() {
   return (
-    <Provider store={store} >
-    <Navbar />
-    <Login />
-  </Provider>
+    <Provider store={store}>
+      <Navbar />
+      <Login />
+    </Provider>
   );
 }
