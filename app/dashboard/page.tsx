1e4580926c76d95148b17ee14026760dcac9cada
Prudence Levison <you@example.com>
2025-01-02 11:07:01 +0100

    notification added successfully

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,75 +1,60 @@
-
-"use client";
+'use client';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../Store/store';
+import { getNotifications } from '../request/getNotification';
 
 interface Notification {
-  id: number;
-  message: string;
-  timestamp: string;
+	id: number;
+	message: string;
+	timestamp: string;
+	body: string;
 }
 
-const accessTokenValue = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJoZW5yeTA0MkB0ZWFtNzA3NzM4LnRlc3RpbmF0b3IuY29tIiwic3RhZmZfZW1haWwiOm51bGwsInN0YWZmX2lkIjpudWxsLCJhY2NvdW50X3R5cGUiOiJJTkRJVklEVUFMIiwiYWNjb3VudF9tb2RlIjoiUkVHSVNURVJFRCIsImlhdCI6MTczNTc2MDA3MiwiZXhwIjoxNzM1OTMyODcyfQ.CfdqnIsP9xit6pvULxzNISdK_pruYtWi9oUKR4T248A"
+const accessTokenValue =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJoZW5yeTA0MkB0ZWFtNzA3NzM4LnRlc3RpbmF0b3IuY29tIiwic3RhZmZfZW1haWwiOm51bGwsInN0YWZmX2lkIjpudWxsLCJhY2NvdW50X3R5cGUiOiJJTkRJVklEVUFMIiwiYWNjb3VudF9tb2RlIjoiUkVHSVNURVJFRCIsImlhdCI6MTczNTc2MDA3MiwiZXhwIjoxNzM1OTMyODcyfQ.CfdqnIsP9xit6pvULxzNISdK_pruYtWi9oUKR4T248A';
 
 export const Dashboard = () => {
+	const [notifications, setNotifications] = useState<Notification[]>([]);
+	const [accessToken, setAccessToken] = useState('');
 
-  const [isInitialized, setIsInitialized] = useState(false);
-  const [notifications, setNotifications] = useState<Notification[]>([]);
-  const [accessToken, setAccessToken] = useState('');
+	const dispatch = useDispatch<AppDispatch>();
 
-  // useEffect(() => {
-    // if (!accessToken) {
-      // setAccessToken(accessTokenValue);
-    // }
-  // }, [accessToken]);
 
-  useEffect(() => {
-    if (!accessToken && !isInitialized) {
-      setAccessToken(accessTokenValue);
-      setIsInitialized(true);
-    } else if (accessToken) {
-      const fetchNotifications = async () => {
-        try {
-          const response = await axios.get('https://api-logic-dev.paidby.app/notification', {
-            headers: {
-              Authorization: `Bearer ${accessTokenValue}`,
-            },
-          });
-          const data = response.data;
-          setNotifications(data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      fetchNotifications();
-    }
-  }, [accessToken, isInitialized]);
-  
-  
-  
-  return (
-    <div>
-      <h1>Notifications</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Message</th>
-            <th>Timestamp</th>
-          </tr>
-        </thead>
-        <tbody>
-          {notifications.map((notification: Notification) => (
-            <tr key={'https://api-logic-dev.paidby.app/notification'}>
-              <td>{'https://api-logic-dev.paidby.app/notification'}</td>
-              <td>{notification.message}</td>
-              <td>{notification.timestamp}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+
+	const fetchNotifications = async () => {
+		const response = await dispatch(getNotifications()).unwrap();
+
+		setNotifications(response?.data?.data?.legacy_v2?.data);
+	};
+
+	useEffect(() => {
+		fetchNotifications();
+	}, []);
+
+	return (
+		<div>
+			<h1>Notifications</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Message</th>
+						<th>Timestamp</th>
+					</tr>
+				</thead>
+				<tbody>
+					{notifications.map((notification: Notification) => (
+						<tr key={notification.id}>
+							<td>{notification.body}</td>
+							<td>{notification.message}</td>
+							<td>{notification.timestamp}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default Dashboard;
--- app/request/getNotification.ts
+++ app/request/getNotification.ts
@@ -1,29 +1,23 @@
 
-import { NextApiRequest, NextApiResponse } from 'next';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const getNotifications = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const accessToken = req.headers.authorization;
-    if (!accessToken) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
+export const getNotifications = createAsyncThunk('notification', async (undefined, thunkAPI) => {
+	try {
+		const userData = window.localStorage.getItem('user')!;
 
-    const response = await fetch('https://api-logic-dev.paidby.app/notification', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+		const accessToken = JSON.parse(userData)?.access_token;
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
+		const response = await axios.get(`https://api-logic-dev.paidby.app/notification?page=1`, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+		});
 
-    const notifications = await response.json();
-    return res.status(200).json(notifications);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-export default getNotifications;
+		return response;
+	} catch (error: any) {
+		const message = error?.response?.data?.message?.toString();
+		return thunkAPI.rejectWithValue(message);
+	}
+  
+});
